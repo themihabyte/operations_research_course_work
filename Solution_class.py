f2a9cf81e5abd9f80ecbdcc59f183957ea141a18
themihabyte <mihabayt@gmail.com>
2021-06-01 18:02:14 +0300

    Delete

--- Solution_class.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def __init__(self, T):
-        self.z = 0
-        self.x = []
-        for i in range(T):
-            self.x.append(0)
-
-    def __str__(self):
-        return "Z = {:.3f}, x = {}".format(self.z, self.x)
---
+++ src/greedy_algorithm.py
@@ -0,0 +1,38 @@
+from Lab_work_class import Lab_work
+from Solution_class import Solution
+import csv
+def greedy_algorithm(T, N, n, p):
+    labs = []
+    for i in range(T):
+        labs.append(Lab_work(p[i], n[i]))
+    solution = Solution(T)
+    A = {}
+    for i in range(N):
+        for j in range(T):
+            if solution.x[j] < n[j]:
+                A[j] = labs[j].P[solution.x[j]]/(solution.x[j]+1)
+        lab_num = list(A.keys())
+        probabilities = list(A.values())
+        a = max(probabilities)
+        best = probabilities.index(a)
+        solution.x[best] += 1
+        solution.z += labs[best].P[solution.x[best]-1]
+        A.clear()
+    return solution
+
+if __name__ == "__main__":
+    T = 0
+    N = 0
+    n = []
+    p = []
+    with open("src/problems.csv", 'r', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if T == 0:
+                T = int(row[0])
+                N = int(row[1])
+            else:
+                n.append(int(row[0]))
+                p.append(float(row[1]))
+    
+    print (greedy_algorithm(T, N, n, p))
