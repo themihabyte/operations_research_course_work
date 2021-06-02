b71c601347d140d254347e1662bb7e145b784d7f
themihabyte <mihabayt@gmail.com>
2021-06-02 21:07:59 +0300

    added eq, str methods

--- src/Solution_class.py
+++ src/Solution_class.py
@@ -7,3 +7,19 @@ class Solution:
 
     def __str__(self):
         return "Z = {:.3f}, x = {}".format(self.z, self.x)
+    
+    def __eq__(self, value):
+        if isinstance(value, Solution):
+            if len(self.x) != len(value.x):
+                return False
+            for i in range(len(self.x)):
+                if self.x[i] != value.x[i]:
+                    return False
+            if self.z != value.z:
+                return False
+            return True
+
+        return NotImplemented
+    
+    def increase_z(self, value):
+        self.z += value
