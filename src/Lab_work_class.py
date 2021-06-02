45017c916f9019e0b3c0dd44f9c85556f75e3378
themihabyte <mihabayt@gmail.com>
2021-06-02 21:07:27 +0300

    for 0 attemts gives 0 probability

--- src/Lab_work_class.py
+++ src/Lab_work_class.py
@@ -6,5 +6,8 @@ class Lab_work:
         self.__calculate_probability_for_try()
     
     def __calculate_probability_for_try(self):
-        for i in range(self.n):
-            self.P.append(1-(1-self.p)**(i+1))
+        for i in range(self.n+1):
+            if i == 0:
+                self.P.append(0)
+            else:
+                self.P.append(1-(1-self.p)**(i))
