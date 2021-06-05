0c624367a6de3204b09b4c9b72dcee59bfd325fc
themihabyte <mihabayt@gmail.com>
2021-06-05 13:44:57 +0300

    Deleted increase z, added compare methods, labs, calculate Z method,

--- src/Solution_class.py
+++ src/Solution_class.py
@@ -1,9 +1,19 @@
 class Solution:
+    labs = []
     def __init__(self, T):
         self.z = 0
         self.x = []
         for i in range(T):
             self.x.append(0)
+
+    def calculate_z(self):
+        if len(self.labs) == 0:
+            return 0
+        self.z = 0
+        for i in range(len(self.labs)):
+            for j in range(self.x[i]+1):
+                self.z += self.labs[i].P[j]
+
 
     def __str__(self):
         return "Z = {:.3f}, x = {}".format(self.z, self.x)
@@ -15,11 +25,18 @@ class Solution:
             for i in range(len(self.x)):
                 if self.x[i] != value.x[i]:
                     return False
-            if self.z != value.z:
-                return False
             return True
 
         return NotImplemented
-    
-    def increase_z(self, value):
-        self.z += value
+
+    def __lt__(self, value):
+        return self.z < value.z
+
+    def __le__(self, value):
+        return self.z <= value.z
+
+    def __gt__(self, value):
+        return self.z > value.z
+
+    def __ge__(self, value):
+        return self.z >= value.z
