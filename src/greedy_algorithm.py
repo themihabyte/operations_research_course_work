553171105ba929a1dc87a0eb968999ecaa2f7d0d
Zhurba Maria <masha.zhurba8@gmail.com>
2021-06-05 20:25:14 +0300

    fixed problem

--- src/greedy_algorithm.py
+++ src/greedy_algorithm.py
@@ -2,22 +2,21 @@ from Lab_work_class import Lab_work
 from Solution_class import Solution
 import csv
 def greedy_algorithm(T, N, n, p):
-    labs = []
     for i in range(T):
-        labs.append(Lab_work(p[i], n[i]))
+        Solution.labs.append(Lab_work(p[i], n[i]))
     solution = Solution(T)
     A = {}
     for i in range(N):
         for j in range(T):
             if solution.x[j] < n[j]:
-                A[j] = labs[j].P[solution.x[j]+1]/(solution.x[j]+1)
+                A[j] = Solution.labs[j].P[solution.x[j]+1]/(solution.x[j]+1)
         lab_num = list(A.keys())
         probabilities = list(A.values())
         a = max(probabilities)
         best = lab_num[probabilities.index(a)]
         solution.x[best] += 1
-        solution.increase_z(labs[best].P[solution.x[best]])
         A.clear()
+    solution.calculate_z()
     return solution
 
 if __name__ == "__main__":
@@ -25,7 +24,7 @@ if __name__ == "__main__":
     N = 0
     n = []
     p = []
-    with open("src/problems.csv", 'r', encoding='utf-8-sig') as csvfile:
+    with open("problems.csv", 'r', encoding='utf-8-sig') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if T == 0:
