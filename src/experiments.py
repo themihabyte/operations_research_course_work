88cb3455775e4720fc5071c20ad1a115b510555e
themihabyte <55716049+themihabyte@users.noreply.github.com>
2021-06-20 23:07:43 +0300

    Add files via upload

--- src/experiments.py
+++ src/experiments.py
@@ -1,7 +1,10 @@
 from genetic_algorithm import genetic_algorithm
+from branches_method import branches_method
 import random
 import math
 import matplotlib.pyplot as plt
+import datetime
+import csv
 def init_p_n(T, N):
     p = []
     n = []
@@ -14,6 +17,12 @@ def init_p_n(T, N):
             n.append(random.randint(int(math.ceil(0.2*N)), int(math.ceil(1.8*N))))
         else:
             n.append(random.randint(round(0.8*N), round(1.2*N)))
+        while sum(n) <= N:
+            n.clear()
+            if random.random()>0.5:
+                n.append(random.randint(int(math.ceil(0.2*N)), int(math.ceil(1.8*N))))
+            else:
+                n.append(random.randint(round(0.8*N), round(1.2*N)))
     return p, n
 
 def iterations():
@@ -21,7 +30,9 @@ def iterations():
         for N in [round(1.5*T), round(3*T), round(5*T)]:
             p, n = init_p_n(T, N)
             for C in [20, 50, 100]:
+                
                 solution = genetic_algorithm(T, N, n, p, 10, C)
+                
                 iteratio = []
                 z = []
                 for i in range(len(solution)):
@@ -33,4 +44,29 @@ def iterations():
                 plt.xlabel("Iteration")
                 plt.savefig("src/figures/iterations/Labs{}_Attempts{}_Iterations{}.png".format(T, N, C), format="png")
 
-iterations()
+def population_size():
+    with open("experiments/population_size.csv", 'w', encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+        writer.writerow(("T", "N", "C"))
+    for T in range(10, 41, 5):
+        for N in [round(1.5*T), round(3*T), round(5*T)]:
+            p, n = init_p_n(T, N)
+            res = genetic_algorithm(T, N, n, p, 10, 1000)
+            optimum = res[len(res)-1].z#branches_method(T, N, n, p)
+            C_opt = 0
+            time_opt = 999999
+            for C in range(5, 105, 5):
+                start_time = datetime.datetime.now()
+                solution = genetic_algorithm(T, N, n, p, C, 20)
+                end_time = datetime.datetime.now()
+                time_diff = end_time - start_time
+                working_time = time_diff.total_seconds() * 1000
+                print(working_time)
+                if time_opt > working_time and optimum <= solution[len(solution)-1].z:
+                    C_opt = C
+                    time_opt = working_time
+            with open("experiments/population_size.csv", 'a', encoding='utf-8-sig') as f:
+                writer = csv.writer(f)
+                writer.writerow((T, N, C_opt))
+
+population_size()
---
+++ src/main.py
@@ -0,0 +1,109 @@
+from genetic_algorithm import genetic_algorithm
+from branches_method import branches_method
+import csv
+import random
+def user_input():
+    print("Input T:")
+    T = int(input())
+    print("Input N:")
+    N = int(input())
+    p = []
+    n = []
+    for i in range(T):
+        print("input nubmer of attempts for lab {}".format(i+1))
+        n.append(int(input()))
+        print("input probability for lab {}".format(i+1))
+        p.append(float(input()))
+    return T, N, n, p
+
+def read_from_file(filename):
+    n = []
+    p = []
+    T = 0
+    N = 0
+    with open(filename, 'r', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if T == 0:
+                T = int(row[0])
+                N = int(row[1])
+            else:
+                n.append(int(row[0]))
+                p.append(float(row[1]))
+    return T, N, n, p
+
+def generate_data(T_lower_bound, T_upper_bound, N_lower_bound, N_upper_bound, 
+n_lower_bound, n_upper_bound, p_lower_bound, p_upper_bound):
+    T = random.randint(T_lower_bound, T_upper_bound)
+    N = random.randint(N_lower_bound, N_upper_bound)
+    while N <= T:
+        N = random.randint(N_lower_bound, N_upper_bound)
+    n = []
+    p = []
+    for i in range(T):
+        n.append(random.randint(n_lower_bound, n_upper_bound))
+        p.append(p_lower_bound+random.random()*(p_upper_bound-p_lower_bound))
+    while sum(n) <= N:
+        n.clear()
+        for i in range(T):
+            n.append(random.randint(n_lower_bound, n_upper_bound))
+    return T, N, n, p
+
+def run_experiments():
+    pass
+
+def input_problem():
+    T = 0
+    N = 0
+    p = []
+    n = []
+    print("""MENU:
+        1) input data
+        2) generate data
+        3) read data from file""")
+    menu = int(input())
+    if menu == 1:
+        T, N, n, p = user_input()
+    elif menu == 2:
+        print("Input lower bound for T")
+        T_lower = int(input())
+        print("Input upper bound for T")
+        T_upper = int(input())
+        print("Input lower bound for N")
+        N_lower = int(input())
+        print("Input upper bound for N")
+        N_upper = int(input())
+        print("Input lower bound for n")
+        n_lower = int(input())
+        print("Input upper bound for n")
+        n_upper = int(input())
+        print("Input lower bound for p")
+        p_lower = float(input())
+        print("Input upper bound for p")
+        p_upper = float(input())
+        T, N, n, p = generate_data(T_lower, T_upper, N_lower,
+        N_upper, n_lower, n_upper, p_lower, p_upper)
+    elif menu == 3:
+        print("Input filename:")
+        filename = input()
+        T, N, n, p = read_from_file(filename)
+    print("T =", T, "\nN =", N, "\nn =", n, "\np =", p)
+    res = genetic_algorithm(T, N, n, p, 5, 1000)  # TODO: Change parameters
+    print("Genetic algorithm: ", res[len(res)-1])
+    print("Branches method: ", branches_method(T, N, n, p))
+
+if __name__ == "__main__":
+    while True:
+        print("""MENU:
+        1) Run experiments
+        2) Input problem
+        3) Exit""")
+        menu = int(input())
+        if menu == 1:
+            run_experiments()
+        elif menu == 2:
+            input_problem()
+        elif menu == 3:
+            break
+        else:
+            print("Try again")
